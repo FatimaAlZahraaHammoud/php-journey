0d03c933fdbc7bfdc1da971310e805e3dd590586
FatimaZah7 <101220079@mu.edu.lb>
2024-11-17 11:14:17 +0200

    Finish exercise user class

---
+++ user.php
@@ -0,0 +1,57 @@
+<?php 
+    class User{
+        private $username;
+        private $email;
+        private $password;
+
+        public function __construct($username, $email, $password){
+            $this->username = $username;
+            $this->email = $email;
+            $this->password = $password;
+        }
+
+        // check password
+        public static function check_password($password){
+            // expression regex
+            return preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*\W).{12,}$/", $password);
+        }
+
+        // validate email
+        public static function validate_email($email){
+            return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;;
+        }
+
+        public function copy_with($username = null, $email = null, $password = null){
+            return new User(
+                $username ?? $this->username,
+                $email ?? $this->email,
+                $password ?? $this->password,
+            );
+        }
+    }
+
+    // Validating password
+    $password = "Hello@Pass123";
+    $password2 = "hello";
+    echo User::check_password($password) ? "Valid Password" : "Invalid Password";
+    echo "\n";
+    echo User::check_password($password2) ? "Valid Password" : "Invalid Password";
+    echo "\n";
+    
+    // Validating email
+    $email = "test@example.com";
+    $email2 = "testexample.com";
+    echo User::validate_email($email) ? "Valid Email" : "Invalid Email";
+    echo "\n";
+    echo User::validate_email($email2) ? "Valid Email" : "Invalid Email";
+    echo "\n";
+
+    // Creating and copying users
+    $user = new User("Fatima", "fatima@example.com", "Hello@Pass123");
+    
+    // Creating a copy with updated username and email
+    $newUser = $user->copy_with(username:"Fatima AlZahraa", email: "fatimaAlZahraa@example.com");
+    
+    print_r($newUser);
+
+?>
